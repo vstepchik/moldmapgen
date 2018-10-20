6b2c2c38345f25cced61636e37d52d69fc57f2b6
Vitalik Stepchik <vitalik.stepchik@omnixlabs.com>
2018-10-20 14:09:22 +0300

    defined plate class

--- geography.py
+++ geography.py
@@ -1,18 +1,24 @@
 import random
-from typing import Set, List
+from colorsys import hsv_to_rgb
+from typing import Set, List, Tuple
 
 import numpy as np
 
 from mesh import Mesh
 
 
+class Plate(Set[int]):
+    velocity: Tuple[float, float] = (0.0, 0.0)
+    color: Tuple[float, float, float] = (0.0, 0.0, 0.0)
+
+
 class Geo:
     def __init__(self, mesh: Mesh) -> None:
         self.mesh = mesh
 
-    def create_tectonic_plates(self, n: int) -> List[Set[int]]:
+    def create_tectonic_plates(self, n: int) -> List[Plate]:
         starting_regions = random.sample(range(len(self.mesh.points)), n)
-        plates = [{p} for p in starting_regions]
+        plates = [Plate([p]) for p in starting_regions]
         taken = {p for point_set in plates for p in point_set}
         spread_probabilities = np.random.uniform(0.3, 0.7, n)
 
@@ -39,5 +45,9 @@ class Geo:
                 to_remove.add(region)
                 to_add |= (neighbors - taken - active_set)
             active_set = (active_set - to_remove) | to_add
+
+        for i, plate in enumerate(plates):
+            plate.velocity = tuple(np.random.uniform(-1.0, 1.0, 2))
+            plate.color = hsv_to_rgb(float(i) / len(plates), 0.9, 0.7)
 
         return plates
